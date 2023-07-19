26a9414a1e0a6266a9eed00b2d3f4913f5aafb7b
Luo Guiwen <luoguiwenchina@gmail.com>
2023-07-19 08:00:51 +0800

    Delete the redundant configuration files

--- ches_config_files/config_file_n_exp_17_beta.h
+++ ches_config_files/config_file_n_exp_17_beta.h
@@ -1,16 +1,16 @@
 /*
-n = 2**16
+n = 2**17
 */
 
-const int N_EXP = 16; 
+// parameters for q/5
+const int N_EXP = 17; 
 const size_t N_POINTS = (size_t) 1<< N_EXP; 
-
-constexpr int EXPONENT_OF_q = 16;
+constexpr int EXPONENT_OF_q = 19;
 constexpr int q_RADIX = (int) (1 << EXPONENT_OF_q);
-constexpr int h_LEN_SCALAR = 16;
-constexpr int a_LEADING_TERM = 29677;
+constexpr int h_LEN_SCALAR = 14;
+constexpr int a_LEADING_TERM = 231;
 constexpr int d_MAX_DIFF = 6;
-constexpr int B_SIZE = 18343;
+constexpr int B_SIZE = 109244;
 
 //parameters for q/2 (BGMW95) 
 const int EXPONENT_OF_q_BGMW95 = 17;
